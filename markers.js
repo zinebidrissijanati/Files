08acfbcbe162c7e15b2c1a2a0e5f4130a1386969
zinebidrissijanati <60932655+zinebidrissijanati@users.noreply.github.com>
2021-03-05 11:56:41 +0100

    Create markers.js

---
+++ markers.js
@@ -0,0 +1,173 @@
+export default  markers = [
+    {
+        "agency": "jinane sais",
+        "phone": "",
+        "address": "",
+        coordinate: {
+            "latitude": "33.953222",
+            "longitude": "-5.031250"
+        }
+    },
+    {
+        "agency": "oued fes",
+        "phone": "0535.75.57.83",
+        "address": "Rue n° 5, Oued Fès",
+        coordinate: {
+            "latitude": "34.053583",
+            "longitude": "-5.030583"
+        }
+    },
+    {
+        "agency": "zohour",
+        "phone": "0535.61.75.82",
+        "address": "Angle rue Ikli et Ourika, Avenue Soussa, Route de Sefrou, Fès",
+        coordinate: {
+            "latitude": "34.005750",
+            "longitude": "-4.985389"
+        }
+    },
+    {
+        "agency": "batha ",
+        "phone": "0535.63.88.73",
+        "address": "N° 3, rue Ahmed Mekouar, Batha",
+        coordinate: {
+            "latitude": "34.059361",
+            "longitude": "-4.980611"
+        }
+    },
+    {
+        "agency": "sidi brahim",
+        "phone": "",
+        "address": "Quartier Industriel Sidi Brahim",
+        coordinate: {
+            "latitude": "34.030222",
+            "longitude": "-4.984000"
+        }
+    },
+    {
+        "agency": "sehb el ouard",
+        "phone": "0535.76.25.95",
+        "address": "Douar Riafa, Sehb Ouard",
+        coordinate: {
+            "latitude": "34.059472",
+            "longitude": "-4.958111"
+        }
+    },
+    {
+        "agency": "rcif",
+        "phone": "0535.66.94.66",
+        "address": "Bab Jdid, Rcif",
+        coordinate: {
+            "latitude": "34.062111",
+            "longitude": "-4.972667"
+        }
+    },
+    {
+        "agency": "fes jdid",
+        "phone": "0535.62.17.37",
+        "address": "Bab Semmarine, Fès Jdid",
+        coordinate: {
+            "latitude": "34.054222,",
+            "longitude": "-4.990139"
+        }
+    },
+    {
+        "agency": "bab khokha",
+        "phone": "0535.64.83.46",
+        "address": "N° 46, Jnane Ben Kirane",
+        coordinate: {
+            "latitude": "34.067556",
+            "longitude": "-4.961028"
+        }
+    },
+    {
+        "agency": "farah",
+        "phone": "0535.60.10.06",
+        "address": "Lotiss Bennis, Route Ain Smen, Hay Farah",
+        coordinate: {
+            "latitude": "34.022889",
+            "longitude": "-5.013306"
+        }
+    },
+    {
+        "agency": "narjiss",
+        "phone": "0535.73.21.27",
+        "address": "Avenue Kairaouane, Narjiss B",
+        coordinate: {
+            "latitude": "34.020167",
+            "longitude": "-4.977639"
+        }
+    },
+    {
+        "agency": "bensouda",
+        "phone": "0535.72.60.60",
+        "address": "Marché de gros Bensouda",
+        coordinate: {
+            "latitude": "34.004111",
+            "longitude": " -5.051639"
+        }
+    },
+    {
+        "agency": "ain haroun",
+        "phone": "0535.64.66.49",
+        "address": "Rue 210, Hay Moufarrih, Ain Haroune",
+        coordinate: {
+            "latitude": "34.065028",
+            "longitude": "-5.005528"
+        }
+    },
+    {
+        "agency": "Delegation sefrou",
+        "phone": "0535.66.00.13",
+        "address": "Route de Boulemane, Sefrou",
+        coordinate: {
+            "latitude": "33.822000",
+            "longitude": " -4.835361"
+        }
+    },
+    {
+        "agency": "dhar lakhmiss",
+        "phone": "0535.63.17.61",
+        "address": "Boulevard Iran, Dhar Lakhmis",
+        coordinate: {
+            "latitude": "34.068333",
+            "longitude": "-4.988306"
+        }
+    },
+    {
+        "agency": "ville nouvelle",
+        "phone": "0535.62.50.15",
+        "address": "10 rue Mohamed El Kaghat B.P. 2097 Fès",
+        coordinate: {
+            "latitude": "34.040889",
+            "longitude": " -5.001722"
+        }
+    },
+    {
+        "agency": "el borj",
+        "phone": "0535.76.46.33",
+        "address": "N° 9, lotissement Borj, Sehrij Gnaoua",
+        coordinate: {
+            "latitude": "34.052778",
+            "longitude": "-4.956361"
+        }
+    },
+    {
+        "agency": "BHALIL",
+        "phone": "0535.69.20.52",
+        "address": "Rue Merj Aarouss Bsila Bhalil",
+        coordinate: {
+            "latitude": "33.856861",
+            "longitude": "-4.871611"
+        }
+    },
+    {
+        "agency": "BIR TAMTAM",
+        "phone": "0535.97.61.32",
+        "address": "Cité ADM Centre Bir Tamtam",
+        coordinate: {
+            "latitude": "33.979167",
+            "longitude": "-4.641667"
+        }
+    },
+]
